edbd6e9d68720bd73492f49514f0b3c31c879178
Christer Blomqvist <christer.blomqvist@gmail.com>
2018-07-17 10:22:47 +0200

    Solve day5, part 2

---
+++ day05/jumplist2.py
@@ -0,0 +1,39 @@
+import argparse
+import mylog
+
+log = mylog.setup()
+
+# This lets us parse argument from the command line
+parser = argparse.ArgumentParser(description="Figure out number of jumps to excape the list in input.txt. See instructions.txt")
+
+# We need to read a file
+parser.add_argument("filename", help="The file to read")
+
+# Let' see what we get from the command line
+args = parser.parse_args()
+
+filename = args.filename
+log.info("The file to read is: {}".format(args.filename))
+
+def caclulate_offset(current):
+    if current >= 3:
+        return current - 1
+    return current + 1
+
+jumplist = []
+with open(filename) as file:
+    # Populate the jumplist with values
+    for line in file:
+        jumplist.append(int(line.strip()))
+
+# Process the jumplist
+index = 0
+jumps = 0
+# We are going to bounce around the list until we have exited it, i.e. gotten to an index outside of the jumplist
+while index >= 0 and index < len(jumplist):
+    nextindex = index + jumplist[index]
+    jumps += 1
+    jumplist[index] = caclulate_offset(jumplist[index])
+    index = nextindex
+
+print("The jumplist of {} steps was cleared in {} jumps and landed you on index {}.".format(len(jumplist), jumps, index))
